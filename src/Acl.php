fb7ab8a3bf5717b9b5e5733aa84b8ba72b750845
Martynas Andriuskevicius <martynas.andriuskevicius@hostinger.com>
2016-04-01 17:20:47 +0300

    Add support for user roles

--- src/Acl.php
+++ src/Acl.php
@@ -10,39 +10,49 @@ class Acl
 
     /**
      * rawConfig
-     * 
+     *
      * (default value: array())
-     * 
+     *
      * @var array
      * @access private
      */
     private $rawConfig = array();
-    
+
     /**
      * roles
-     * 
+     *
      * (default value: array())
-     * 
+     *
      * @var array
      * @access private
      */
     private $roles = array();
-    
+
+    /**
+     * user roles
+     *
+     * (default value: array())
+     *
+     * @var array
+     * @access private
+     */
+    private $userRoles = array();
+
     /**
      * resources
-     * 
+     *
      * (default value: array())
-     * 
+     *
      * @var array
      * @access private
      */
     private $resources = array();
-    
+
     /**
      * access
-     * 
+     *
      * (default value: array())
-     * 
+     *
      * @var array
      * @access private
      */
@@ -50,7 +60,7 @@ class Acl
 
     /**
      * __construct function.
-     * 
+     *
      * @access public
      * @param string $config (default: "")
      * @return void
@@ -61,10 +71,10 @@ class Acl
             $this->readConfig($config);
         }
     }
-    
+
     /**
      * readConfig function.
-     * 
+     *
      * @access public
      * @param string $config (default: "")
      * @return array
@@ -79,23 +89,37 @@ class Acl
             if (!file_exists($filename)) {
                 throw new Exception(sprintf('Config file "%s" not found.', $filename), 10002);
             }
-            
+
             $this->rawConfig = json_decode(file_get_contents($filename), 1);
 
             if (!$this->rawConfig) {
                 throw new Exception(sprintf('Config file "%s" failed to parse.', $filename), 10002);
             }
+
             $this->parseRoles();
+            $this->parseUserRoles();
             $this->parseRecursiveResources($this->rawConfig['resources']);
             $this->parseAccessFromResources();
         }
-        
+
         return $this->rawConfig;
     }
-    
+
+    /**
+     * Parses user roles
+     *
+     * @access private
+     * @return void
+     */
+    public function parseUserRoles(){
+        if(isset($this->rawConfig['users']) && is_array($this->rawConfig['users'])){
+            $this->userRoles = $this->rawConfig['users'];
+        }
+    }
+
     /**
      * parseRoles function.
-     * 
+     *
      * @access private
      * @return void
      */
@@ -110,7 +134,7 @@ class Acl
 
     /**
      * parseAccessFromResources function.
-     * 
+     *
      * @access private
      * @return void
      */
@@ -121,17 +145,17 @@ class Acl
                 if (!isset($this->access[$role])) {
                     $this->access[$role] = array();
                 }
-                
+
                 if (!in_array($resource, $this->access[$role])) {
                     $this->access[$role][] = $resource;
                 }
             }
         }
     }
-    
+
     /**
      * parseRecursiveResources function.
-     * 
+     *
      * @access private
      * @param mixed $arr
      * @param array $narr (default: array())
@@ -153,23 +177,23 @@ class Acl
                     }
                 }
             }
-            
+
             $this->resources[$nkey . $key] = $access;
 
-            
+
             if (isset($value['children']) && is_array($value['children'])) {
                 $narr = array_merge($narr, $this->parseRecursiveResources($value['children'], $narr, $access, $nkey . $key . '.'));
             } else {
                 $narr[$nkey . $key] = $value;
             }
         }
-        
+
         return $arr;
     }
-    
+
     /**
      * checkIfRoleExists function.
-     * 
+     *
      * @access private
      * @param Role $role
      * @return boolean
@@ -178,10 +202,10 @@ class Acl
     {
         return in_array($role, $this->roles, true);
     }
-    
+
     /**
      * checkIfResourceExists function.
-     * 
+     *
      * @access private
      * @param mixed $resource
      * @return boolean
@@ -191,13 +215,13 @@ class Acl
         if (isset($this->resources[$resource])) {
             return true;
         }
-        
+
         return false;
     }
-    
+
     /**
      * checkIfRoleHasAccessToResource function.
-     * 
+     *
      * @access private
      * @param mixed $role
      * @param mixed $resource
@@ -210,10 +234,10 @@ class Acl
         }
         return in_array($resource, $this->access[$role], true);
     }
-        
+
     /**
      * check function.
-     * 
+     *
      * @access public
      * @param Role $role
      * @param Resource $resource
@@ -224,21 +248,35 @@ class Acl
         if (!$this->checkIfRoleExists($role)) {
             return false;
         }
-        
+
         if (!$this->checkIfResourceExists($resource)) {
             return false;
         }
-        
+
         if (!$this->checkIfRoleHasAccessToResource($role, $resource)) {
             return false;
         }
-        
+
         return true;
     }
-    
+
+    /**
+     * Checks if the specified user has access
+     *
+     * @param $user
+     * @param $resource
+     * @return bool
+     */
+    public function checkUser($user, $resource){
+        if(!isset($this->userRoles[$user])){
+            return false;
+        }
+        return $this->check($this->userRoles[$user], $resource);
+    }
+
     /**
      * enforce function.
-     * 
+     *
      * @access public
      * @param mixed $role
      * @param mixed $resource
@@ -249,21 +287,21 @@ class Acl
         if (!$this->checkIfRoleExists($role)) {
             throw new Exception(sprintf('Role "%s" not found', $role), 11001);
         }
-        
+
         if (!$this->checkIfResourceExists($resource)) {
             throw new Exception(sprintf('Resource "%s" not found', $resource), 11002);
         }
-        
+
         if (!$this->checkIfRoleHasAccessToResource($role, $resource)) {
             throw new Exception(sprintf('Role "%s" has no access to resource "%s".', $role, $resource), 11003);
         }
-        
+
         return true;
     }
-    
+
     /**
      * getRoles function.
-     * 
+     *
      * @access public
      * @return array
      */
@@ -271,10 +309,10 @@ class Acl
     {
         return $this->roles;
     }
-    
+
     /**
      * getResources function.
-     * 
+     *
      * @access public
      * @return array
      */
@@ -282,10 +320,10 @@ class Acl
     {
         return $this->resources;
     }
-    
+
     /**
      * getAccess function.
-     * 
+     *
      * @access public
      * @return array
      */
